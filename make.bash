be369774bf1f16511db8e4a8901674d87245b76c
Julian Chu <nightmare0107@gmail.com>
2021-01-30 10:33:45 +0100

    add clean command, format make.bash

--- make.bash
+++ make.bash
@@ -2,7 +2,7 @@
 
 function help() {
     echo "---- Project: Ptt-backend ----"
-	  echo " Usage: ./make.bash COMMAND"
+    echo " Usage: ./make.bash COMMAND"
     echo
     echo " Management Commands:"
     echo "  build              Build project"
@@ -11,45 +11,46 @@ function help() {
     echo "  lint               Run golangci-lint check"
     echo "  test-unit          Run all unit tests"
     echo "  test-integration   Run all integration and unit tests"
+    echo "  clean              Remove object files, ./bin, .out files"
     echo
 }
 
-function build(){
-  VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
-  BUILDTIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-  GITSHA=$(git rev-parse --short HEAD 2>/dev/null)
-  if [ -z "$VERSION" ]; then \
-    VERSION="git-$GITSHA"
-  fi
+function build() {
+    VERSION=$(git describe --tags $(git rev-list --tags --max-count=1) 2>/dev/null)
+    BUILDTIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    GITSHA=$(git rev-parse --short HEAD 2>/dev/null)
+    if [ -z "$VERSION" ]; then
+        VERSION="git-$GITSHA"
+    fi
 
-  GOFLAGS="-trimpath"
-  LDFLAGS="-X main/version.version=$VERSION -X main/version.commit=$GITSHA -X main/version.buildTime=$BUILDTIME"
-  mkdir -p "bin"
-  echo "binary file output into ./bin"
-  go build "$GOFLAGS" -ldflags "$LDFLAGS" -o ./bin ./...
+    GOFLAGS="-trimpath"
+    LDFLAGS="-X main/version.version=$VERSION -X main/version.commit=$GITSHA -X main/version.buildTime=$BUILDTIME"
+    mkdir -p "bin"
+    echo "binary file output into ./bin"
+    go build "$GOFLAGS" -ldflags "$LDFLAGS" -o ./bin ./...
 }
 
 function format() {
     files=$(find . -path ./vendor -prune -o -name "*.go" -print)
-	  gofmt -s -w $files
+    gofmt -s -w $files
 }
 
 function lint() {
     GOBIN=$(go env GOBIN)
-	  if [ ! -f "$GOBIN/golangci-lint" ]; then \
-		  curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$GOBIN" "$GOLANGCI_LINT_VERSION"; \
-		  echo "download golangci-lint into $GOBIN" ;\
-   	fi;
-   	go vet ./...
-	  echo "golangci-lint checking..."
-	  "$GOBIN"/golangci-lint run --deadline=30m --enable=misspell --enable=gosec --enable=gofmt --enable=goimports --enable=golint ./cmd/... ./...
+    if [ ! -f "$GOBIN/golangci-lint" ]; then
+        curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$GOBIN" "$GOLANGCI_LINT_VERSION"
+        echo "download golangci-lint into $GOBIN"
+    fi
+    go vet ./...
+    echo "golangci-lint checking..."
+    "$GOBIN"/golangci-lint run --deadline=30m --enable=misspell --enable=gosec --enable=gofmt --enable=goimports --enable=golint ./cmd/... ./...
 }
 # no arguments
-if [ $# -lt 1 ];then
+if [ $# -lt 1 ]; then
     help
     exit 0
 # number of arguments greater than 1
-elif [ $# -gt 1 ];then
+elif [ $# -gt 1 ]; then
     echo "invalid args, please check command"
     help
     exit 0
@@ -65,8 +66,8 @@ build)
     ;;
 # deps: Ensures fresh go.mod and go.sum for dependencies
 deps)
-	  go mod tidy
-	  go mod verify
+    go mod tidy
+    go mod verify
     ;;
 # format: Formats Go code
 format)
@@ -78,16 +79,22 @@ lint)
     ;;
 # test-unit: Run all unit tests
 test-unit)
-	  go test -v -cover . ./...
+    CGO_ENABLED=1 && go test -v -coverprofile=coverage.out -cover -race
     ;;
 # test-integration: Run all integration and unit tests
 test-integration)
-    echo 'mode: atomic' > coverage.out
+    echo 'mode: atomic' >coverage.out
     go list ./... | xargs -n1 -I{} sh -c 'go test -race -tags=integration -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.out || exit 255'
     rm coverage.tmp
+    ;;
+# clean: Remove object files, ./bin, .out files
+clean)
+    go clean -i -x
+    echo "rm -rf ./bin *.out"
+    rm -rf ./bin *.out
     ;;
 *)
     echo "invalid args, please check command"
     help
+    ;;
 esac
-
