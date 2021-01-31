61e7e02eb9d7fbdd1e9fa6ae4f5b08ae1db568de
Marko Peng <marko958m@gmail.com>
2021-01-31 13:58:33 +0800

    refactor: rename

--- internal/mock/usecase.go
+++ internal/mock/usecase.go
@@ -15,41 +15,41 @@ func NewMockUsecase() *MockUsecase {
 	return &MockUsecase{}
 }
 
-func (u *MockUsecase) GetUserByID(ctx context.Context, userID string) (bbs.UserRecord, error) {
+func (usecase *MockUsecase) GetUserByID(ctx context.Context, userID string) (bbs.UserRecord, error) {
 	panic("Not implemented")
 }
-func (u *MockUsecase) GetUserFavorites(ctx context.Context, userID string) ([]interface{}, error) {
+func (usecase *MockUsecase) GetUserFavorites(ctx context.Context, userID string) ([]interface{}, error) {
 	panic("Not implemented")
 }
-func (u *MockUsecase) GetUserInformation(ctx context.Context, userID string) (map[string]interface{}, error) {
-	panic("Not implemented")
-}
-
-func (u *MockUsecase) GetBoardByID(ctx context.Context, userID string) (bbs.UserRecord, error) {
-	panic("Not implemented")
-}
-func (u *MockUsecase) GetBoards(ctx context.Context, userID string) []bbs.BoardRecord {
-	panic("Not implemented")
-}
-func (u *MockUsecase) GetClasses(ctx context.Context, userID, classID string) []bbs.BoardRecord {
-	panic("Not implemented")
-}
-func (u *MockUsecase) GetBoardArticles(ctx context.Context, boardID string) []interface{} {
-	panic("Not implemented")
-}
-func (u *MockUsecase) GetBoardArticle(ctx context.Context, boardID, filename string) ([]byte, error) {
-	panic("Not implemented")
-}
-func (u *MockUsecase) GetBoardTreasures(ctx context.Context, boardID string, treasuresID []string) []interface{} {
+func (usecase *MockUsecase) GetUserInformation(ctx context.Context, userID string) (map[string]interface{}, error) {
 	panic("Not implemented")
 }
 
-func (u *MockUsecase) CreateAccessTokenWithUsername(username string) string {
+func (usecase *MockUsecase) GetBoardByID(ctx context.Context, userID string) (bbs.UserRecord, error) {
 	panic("Not implemented")
 }
-func (u *MockUsecase) GetUserIdFromToken(token string) (string, error) {
+func (usecase *MockUsecase) GetBoards(ctx context.Context, userID string) []bbs.BoardRecord {
 	panic("Not implemented")
 }
-func (u *MockUsecase) CheckPermission(token string, permissionId []usecase.Permission, userInfo map[string]string) error {
+func (usecase *MockUsecase) GetClasses(ctx context.Context, userID, classID string) []bbs.BoardRecord {
+	panic("Not implemented")
+}
+func (usecase *MockUsecase) GetBoardArticles(ctx context.Context, boardID string) []interface{} {
+	panic("Not implemented")
+}
+func (usecase *MockUsecase) GetBoardArticle(ctx context.Context, boardID, filename string) ([]byte, error) {
+	panic("Not implemented")
+}
+func (usecase *MockUsecase) GetBoardTreasures(ctx context.Context, boardID string, treasuresID []string) []interface{} {
+	panic("Not implemented")
+}
+
+func (usecase *MockUsecase) CreateAccessTokenWithUsername(username string) string {
+	panic("Not implemented")
+}
+func (usecase *MockUsecase) GetUserIdFromToken(token string) (string, error) {
+	panic("Not implemented")
+}
+func (usecase *MockUsecase) CheckPermission(token string, permissionId []usecase.Permission, userInfo map[string]string) error {
 	panic("Not implemented")
 }
