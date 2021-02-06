13699a5276d88f3848d5599607b74554e975754b
VladkaHuang <sunsky11881@gmail.com>
2021-02-06 23:55:27 +0800

    Add delivery test

---
+++ internal/delivery/http/route_boards_articles_test.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Ptt-official-app/Ptt-backend/internal/mock"
+)
+
+func TestGetBoardArticlesBadRequest(t *testing.T) {
+	userID := "id"
+	usecase := mock.NewMockUsecase()
+	delivery := NewHTTPDelivery(usecase)
+
+	title := ""
+	author := ""
+	recommendCountGe := "qwerty"
+	url := fmt.Sprintf("/v1/boards/test/articles?title=%s&author=%s&recommend_count_ge=%s", title, author, recommendCountGe)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := usecase.CreateAccessTokenWithUsername(userID)
+	t.Logf("testing token: %v", token)
+	req.Header.Add("Authorization", "bearer "+token)
+
+	rr := httptest.NewRecorder()
+	r := http.NewServeMux()
+	r.HandleFunc("/v1/boards/", delivery.routeBoards)
+	r.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+}
