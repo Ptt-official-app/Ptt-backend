1c583ef066c41e20766ed366bb869251434907dd
Pichu Chen <pichu@tih.tw>
2021-02-23 14:36:05 +0800

    update article url
    Signed-off-by: Pichu Chen <pichu@tih.tw>

--- internal/usecase/board.go
+++ internal/usecase/board.go
@@ -156,7 +156,7 @@ func (usecase *usecase) shouldShowOnUserLevel(board bbs.BoardRecord, userID stri
 }
 
 func getArticleURL(boardId string, filename string) string {
-	return fmt.Sprintf("https://ptt-app-dev-codingman.pichuchen.tw/bbs/%s/%s.html", boardId, filename)
+	return fmt.Sprintf("https://pttapp.cc/bbs/%s/%s.html", boardId, filename)
 }
 
 func searchArticles(fileHeaders []bbs.ArticleRecord, cond *ArticleSearchCond) []bbs.ArticleRecord {
