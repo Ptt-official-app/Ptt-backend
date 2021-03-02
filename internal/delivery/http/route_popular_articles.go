d06e25a36c42e9d3c3c065267232c6f2b8b509d7
mclin <karenlin1023@gmail.com>
2021-03-02 21:00:07 +0900

    replace method with GetPopularArticles()

--- internal/delivery/http/route_popular_articles.go
+++ internal/delivery/http/route_popular_articles.go
@@ -3,16 +3,14 @@ package http
 import (
 	"context"
 	"encoding/json"
-	"github.com/Ptt-official-app/Ptt-backend/internal/usecase"
 	"net/http"
 )
 
 func (delivery *httpDelivery) getPopularArticles(w http.ResponseWriter, r *http.Request) {
 	delivery.logger.Debugf("getPopularArticles: %v", r)
-	// TODO: replace GetBoardArticles method when GetPopularArticles usecase is ready
 	responseMap := map[string]interface{}{
 		"data": map[string]interface{}{
-			"items": delivery.usecase.GetBoardArticles(context.Background(), "ALLPOST", &usecase.ArticleSearchCond{}),
+			"items": delivery.usecase.GetPopularArticles(context.Background()),
 		},
 	}
 	b, _ := json.MarshalIndent(responseMap, "", "  ")
