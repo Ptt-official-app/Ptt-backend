0e232660e81734fdb73f08503fa1312f6c4c6da3
mclin <karenlin1023@gmail.com>
2021-03-02 21:06:20 +0900

    fix test failed

--- internal/delivery/http/route_popular_articles.go
+++ internal/delivery/http/route_popular_articles.go
@@ -8,9 +8,21 @@ import (
 
 func (delivery *httpDelivery) getPopularArticles(w http.ResponseWriter, r *http.Request) {
 	delivery.logger.Debugf("getPopularArticles: %v", r)
+	articles, err := delivery.usecase.GetPopularArticles(context.Background())
+	if err != nil {
+		delivery.logger.Errorf("find popular articles failed: %v", err)
+		m := map[string]string{
+			"error":             "find_popular_articles_error",
+			"error_description": "get popular articles failed",
+		}
+		b, _ := json.MarshalIndent(m, "", "  ")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(b)
+		return
+	}
 	responseMap := map[string]interface{}{
 		"data": map[string]interface{}{
-			"items": delivery.usecase.GetPopularArticles(context.Background()),
+			"items": articles,
 		},
 	}
 	b, _ := json.MarshalIndent(responseMap, "", "  ")
