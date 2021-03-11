f71290d189775dcdbfea5aa95af8c2a9049dc801
Lester <sonorous.lester@gmail.com>
2021-03-11 18:34:12 +0800

    Check Response Body

--- internal/delivery/http/route_boards_treasures_test.go
+++ internal/delivery/http/route_boards_treasures_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,12 +17,38 @@ func TestFetchBoardsTreasures(t *testing.T) {
 
 	rr := httptest.NewRecorder()
 	r := http.NewServeMux()
-	r.HandleFunc("v1/boards/", delivery.routeBoards)
+	r.HandleFunc("v1/boards", delivery.routeBoards)
 	r.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
+	}
+
+	responseMap := map[string][]interface{}{}
+	json.Unmarshal(rr.Body.Bytes(), &responseMap)
+	responseData := responseMap["data"]
+	for _, treasures := range responseData {
+		treasure := treasures.(map[string]interface{})
+		if _, ok := treasure["filename"]; !ok {
+			t.Errorf("returned body filename not found.")
+			return
+		}
+		if _, ok := treasure["post_date"]; !ok {
+			t.Errorf("returned body post_date not found.")
+			return
+		}
+		if _, ok := treasure["title"]; !ok {
+			t.Errorf("returned body title not found.")
+			return 
+		}
+		if _, ok := treasure["owner"]; !ok {
+			t.Errorf("returned body owner not found")
+			return
+		}
+		if _, ok := treasure["url"]; !ok {
+			t.Errorf("returned body url not found")
+		}
 	}
 
 
