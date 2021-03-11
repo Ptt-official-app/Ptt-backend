36c53669f66cb2379fb1f96d49acd6ae31fe4a9a
Lester <sonorous.lester@gmail.com>
2021-03-11 16:18:23 +0800

    Create New Request

--- internal/delivery/http/route_boards_treasures_test.go
+++ internal/delivery/http/route_boards_treasures_test.go
@@ -1,3 +1,16 @@
 package http
 
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
+func TestFetchBoardsTreasures(t *testing.T) {
+	usecase := NewMockUsecase()
+	delivery := NewHTTPDelivery(usecase)
+	req, err := http.NewRequest("GET", "/v1/boards/1/treasures/D333/articles", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
