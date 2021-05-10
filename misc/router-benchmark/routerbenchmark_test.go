f250d04c9403c18f841ce53c22c401408c9be5ab
Yu-Lang Chu <yulang.chu@gmail.com>
2021-05-11 00:46:50 +0200

    add ServeMux benchmark

---
+++ misc/router-benchmark/routerbenchmark_test.go
@@ -0,0 +1,103 @@
+package router_benchmark
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func BenchmarkServeMux(b *testing.B) {
+	rr := httptest.NewRecorder()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/boards/", getBoards)
+
+	req, err := http.NewRequest("GET", "/v1/boards/SYSOP/articles", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		mux.ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			b.Errorf("handler returned wrong status code: got %v want %v",
+				rr.Code, http.StatusOK)
+		}
+	}
+}
+
+// getBoards is the handler for `/v1/boards` with GET method
+func getBoards(w http.ResponseWriter, r *http.Request) {
+	boardID, item, filename, _ := parseBoardPath(r.URL.Path)
+	if boardID == "" {
+		//delivery.getBoardList(w, r)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	// get single board
+	if item == "information" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if item == "settings" {
+		//delivery.getBoardSettings(w, r, boardID)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if item == "articles" {
+		if filename == "" {
+			getBoardArticles(w, r, boardID)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		return
+	} else if item == "treasures" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// 404
+	w.WriteHeader(http.StatusNotFound)
+}
+
+// parseBoardPath covert url path from /v1/boards/SYSOP/article to
+// {SYSOP, article) or /v1/boards to {,}
+func parseBoardPath(path string) (boardID string, item string, filename string, err error) {
+	pathSegment := strings.Split(path, "/")
+
+	if len(pathSegment) >= 6 {
+		// /{{version}}/boards/{{class_id}}/{{item}}/{{filename}}
+		boardID = pathSegment[3]
+		item = pathSegment[4]
+		filename = pathSegment[5]
+		return
+	} else if len(pathSegment) == 5 {
+		// /{{version}}/boards/{{class_id}}/{{item}}
+		boardID = pathSegment[3]
+		item = pathSegment[4]
+		return
+	} else if len(pathSegment) == 4 {
+		// /{{version}}/boards/{{class_id}}
+		boardID = pathSegment[3]
+		return
+	} else if len(pathSegment) == 3 {
+		// /{{version}}/boards
+		// Should not be reach...
+		return
+	}
+	return
+}
+
+// getBoardArticles handles request with `/v1/boards/SYSOP/articles` and will return
+// article list to client
+func getBoardArticles(w http.ResponseWriter, r *http.Request, boardID string) {
+	_ = boardID
+	w.WriteHeader(200)
+}
+
+//
+//// getBoards is the handler for `/v1/boards` with GET method
+//func getBoardArticles_Router(w http.ResponseWriter, r *http.Request) {
+//	params := httprouter.ParamsFromContext(r.Context())
+//	boardID := params["boardID"]
+//	_ = boardID
+//	w.WriteHeader(200)
+//}
