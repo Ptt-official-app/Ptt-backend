1d85bef3cf0633bbffdf913842c2ef3bde1da894
Pichu Chen <pichubaby@gmail.com>
2026-08-12 11:31:06 +0800

    test: verify public popular articles without token

--- testing-script/C7-1-1.sh
+++ testing-script/C7-1-1.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # C7-1-1
-# 在不放入Access Token 的情況下 GET /v1/popular-articles 應該出現熱門文章列表，上限為 100 個文章
+# 在不放入 Access Token 的情況下 GET /v1/popular-articles 應該出現熱門文章列表，上限為 100 個文章
 
-ACCESS_TOKEN=`./get_sysop_token.sh`
-curl -s http://localhost:8081/v1/popular-articles -H "Authorization: bearer $ACCESS_TOKEN" 
+curl -s http://localhost:8081/v1/popular-articles
 echo ""
-echo "上限為 100 個看板"
-curl -s http://localhost:8081/v1/popular-articles -H "Authorization: bearer $ACCESS_TOKEN" | jq '.data[] | length '
-
+echo "熱門文章上限為 100 篇"
+curl -s http://localhost:8081/v1/popular-articles | jq '.data.items | length'
