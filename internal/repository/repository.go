ea1ddcdd5266a8036d72ff92feacac4c452a2888
Pichu Chen <pichubaby@gmail.com>
2026-08-12 12:03:20 +0800

    style: preserve repository interface comments

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -22,22 +22,41 @@ type Repository interface {
 	GetBoardArticleRecords(ctx context.Context, boardID string, offset, length uint) ([]bbs.ArticleRecord, error)
 	// GetBoardTreasureRecords returns treasure article records of a board
 	GetBoardTreasureRecords(ctx context.Context, boardID string, treasureIDs []string) ([]bbs.ArticleRecord, error)
+	// GetBoardPostsLimit returns posts limited record of a board
+	// TODO: replace PostsLimitedBoardRecord with real bbs record
+	// GetBoardPostsLimit(ctx context.Context, boardID string) (PostsLimitedBoardRecord, error)
 
 	// user.go
+	// GetUsers returns all user records
 	GetUsers(ctx context.Context) ([]bbs.UserRecord, error)
+	// GetUserFavoriteRecords returns favorite records of a user
 	GetUserFavoriteRecords(ctx context.Context, userID string) ([]bbs.FavoriteRecord, error)
+	// GetUserArticles returns user's articles
 	GetUserArticles(ctx context.Context, boardID string) ([]bbs.ArticleRecord, error)
+	// GetUserPreferences returns user's preferences
+	// TODO: replace UserPreferencesRecord with real bbs record
 	GetUserPreferences(ctx context.Context, userID string) (map[string]string, error)
+	// GetUserComments return user's history comments
+	// TODO: return a slice of concrete type not interface
 	GetUserComments(ctx context.Context, userID string) ([]bbs.UserCommentRecord, error)
+	// GetUserDrafts returns user's draft according to draftID
 	GetUserDrafts(ctx context.Context, userID, draftID string) (bbs.UserDraft, error)
+	// UpdateUserDraft updates user's draft according to draftID
 	UpdateUserDraft(ctx context.Context, userID, draftID string, text []byte) (bbs.UserDraft, error)
+	// DeleteUserDraft deletes user's draft according to draftID
 	DeleteUserDraft(ctx context.Context, userID, draftID string) error
 
 	// article.go
+	// GetPopularArticles returns up to 100 popular articles from public boards
 	GetPopularArticles(ctx context.Context) ([]PopularArticleRecord, error)
+	// AppendComment returns comment details
 	AppendComment(ctx context.Context, userID, boardID, filename, appendType, text string) (PushRecord, error)
+	// CreateArticle
+	// TODO: return result from bbs response
 	CreateArticle(ctx context.Context, userID, boardID, title, article string) (bbs.ArticleRecord, error)
+	// GetRawArticle
 	GetRawArticle(boardID, filename string) (string, error)
+	// ForwardArticleToBoard returns forwarding to board results
 	ForwardArticleToBoard(ctx context.Context, userID, boardID, filename, boardName string) (ForwardArticleToBoardRecord, error)
 }
 
