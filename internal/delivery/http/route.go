0e183a77d6d809036e4fbcf6c112617fa10f2ab2
Pichu Chen <pichubaby@gmail.com>
2026-08-12 12:02:52 +0800

    style: keep board route change focused

--- internal/delivery/http/route.go
+++ internal/delivery/http/route.go
@@ -13,6 +13,7 @@ func (delivery *Delivery) buildRoute(mux *http.ServeMux) {
 	mux.HandleFunc("/v1/popular-articles", delivery.routePopularArticles)
 	mux.HandleFunc("/v1/classes/", delivery.routeClasses)
 	mux.HandleFunc("/v1/users/", delivery.routeUsers)
+
 	mux.HandleFunc("/", delivery.notFoundHandler)
 }
 
@@ -25,11 +26,12 @@ func (delivery *Delivery) notFoundHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (delivery *Delivery) routeToken(w http.ResponseWriter, r *http.Request) {
+	// TODO: Check IP Flowspeed
 	switch r.Method {
 	case http.MethodPost:
 		delivery.postToken(w, r)
 	default:
-		w.Header().Set("Allow", http.MethodPost)
+		w.Header().Set("Allow", strings.Join([]string{http.MethodPost}, ","))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		_, err := w.Write(NewMethodNotAllowedError(r, []string{http.MethodPost}))
 		if err != nil {
@@ -38,8 +40,10 @@ func (delivery *Delivery) routeToken(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// routeBoards is the handler for `/v1/boards`
 func (delivery *Delivery) routeBoards(w http.ResponseWriter, r *http.Request) {
 	delivery.logger.Debugf("routeBoards: %v", r)
+	// TODO: Check IP Flowspeed
 	switch r.Method {
 	case http.MethodGet:
 		delivery.getBoards(w, r)
@@ -56,11 +60,12 @@ func (delivery *Delivery) routeBoards(w http.ResponseWriter, r *http.Request) {
 }
 
 func (delivery *Delivery) routePopularBoards(w http.ResponseWriter, r *http.Request) {
+	// TODO: Check IP Flowspeed
 	switch r.Method {
 	case http.MethodGet:
 		delivery.getPopularBoardList(w, r)
 	default:
-		w.Header().Set("Allow", http.MethodGet)
+		w.Header().Set("Allow", strings.Join([]string{http.MethodGet}, ","))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		_, err := w.Write(NewMethodNotAllowedError(r, []string{http.MethodGet}))
 		if err != nil {
@@ -69,12 +74,13 @@ func (delivery *Delivery) routePopularBoards(w http.ResponseWriter, r *http.Requ
 	}
 }
 
+// routePopularArticles a handler for `/v1/popular-articles`
 func (delivery *Delivery) routePopularArticles(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		delivery.getPopularArticles(w, r)
 	default:
-		w.Header().Set("Allow", http.MethodGet)
+		w.Header().Set("Allow", strings.Join([]string{http.MethodGet}, ","))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		_, err := w.Write(NewMethodNotAllowedError(r, []string{http.MethodGet}))
 		if err != nil {
@@ -83,12 +89,14 @@ func (delivery *Delivery) routePopularArticles(w http.ResponseWriter, r *http.Re
 	}
 }
 
+// routeClasses is the handler for `/v1/classes`
 func (delivery *Delivery) routeClasses(w http.ResponseWriter, r *http.Request) {
+	// TODO: Check IP Flowspeed
 	switch r.Method {
 	case http.MethodGet:
 		delivery.getClasses(w, r)
 	default:
-		w.Header().Set("Allow", http.MethodGet)
+		w.Header().Set("Allow", strings.Join([]string{http.MethodGet}, ","))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		_, err := w.Write(NewMethodNotAllowedError(r, []string{http.MethodGet}))
 		if err != nil {
@@ -97,7 +105,9 @@ func (delivery *Delivery) routeClasses(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// routeClasses is the handler for `/v1/users`
 func (delivery *Delivery) routeUsers(w http.ResponseWriter, r *http.Request) {
+	// TODO: Check IP Flowspeed
 	delivery.logger.Debugf("routeUsers: %v", r)
 	switch r.Method {
 	case http.MethodGet:
@@ -114,29 +124,29 @@ func (delivery *Delivery) routeUsers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// getBoards is the handler for `/v1/boards` with GET method
 func (delivery *Delivery) getBoards(w http.ResponseWriter, r *http.Request) {
+	delivery.logger.Debugf("getBoards: %v", r)
 	boardID, item, filename, err := delivery.parseBoardPath(r.URL.Path)
 	if boardID == "" {
 		delivery.getBoardList(w, r)
 		return
 	}
+	// get single board
 	if item == "information" {
 		delivery.getBoardInformation(w, r, boardID)
 		return
-	}
-	if item == "settings" {
+	} else if item == "settings" {
 		delivery.getBoardSettings(w, r, boardID)
 		return
-	}
-	if item == "articles" {
+	} else if item == "articles" {
 		if filename == "" {
 			delivery.getBoardArticles(w, r, boardID)
 		} else {
 			delivery.getBoardArticlesFile(w, r, boardID, filename)
 		}
 		return
-	}
-	if item == "treasures" {
+	} else if item == "treasures" {
 		delivery.getBoardTreasures(w, r, boardID)
 		return
 	}
@@ -144,12 +154,15 @@ func (delivery *Delivery) getBoards(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	_, err2 := w.Write(NewPathNotFoundError(r))
 	if err2 != nil {
-		delivery.logger.Errorf("write NewPathNotFoundError error: %w", err2)
+		delivery.logger.Errorf("write NewPathNotFoundError error: %w", err)
 	}
+
 	delivery.logger.Noticef("board id: %v not exist but be queried, info: %v err: %v", boardID, item, err)
 }
 
+// postBoards is the handler for `/v1/boards` with POST method
 func (delivery *Delivery) postBoards(w http.ResponseWriter, r *http.Request) {
+	delivery.logger.Debugf("postBoards: %v", r)
 	boardID, item, filename, err := delivery.parseBoardPath(r.URL.Path)
 
 	if boardID == "" && item == "" && filename == "" {
@@ -160,22 +173,23 @@ func (delivery *Delivery) postBoards(w http.ResponseWriter, r *http.Request) {
 	action := r.PostFormValue("action")
 	if action == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		_, writeErr := w.Write(NewPathNotFoundError(r))
-		if writeErr != nil {
-			delivery.logger.Errorf("postBoards write error response err: %w", writeErr)
+		b := NewNewBoardError(r)
+		_, err = w.Write(b)
+		if err != nil {
+			delivery.logger.Errorf("postBoards write error response err: %w", err)
 		}
+
 		return
 	}
 
 	if item == "articles" && boardID != "" {
-		switch {
-		case action == "append_comment" && filename != "":
+		if action == "append_comment" && filename != "" {
 			delivery.appendComment(w, r, boardID, filename)
 			return
-		case action == "forward_article" && filename != "":
+		} else if action == "forward_article" && filename != "" {
 			delivery.forwardArticle(w, r, boardID, filename)
 			return
-		case action == "add_article":
+		} else if action == "add_article" {
 			delivery.publishPost(w, r, boardID)
 			return
 		}
@@ -184,54 +198,83 @@ func (delivery *Delivery) postBoards(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	_, err2 := w.Write(NewPathNotFoundError(r))
 	if err2 != nil {
-		delivery.logger.Errorf("write NewPathNotFoundError error: %w", err2)
+		delivery.logger.Errorf("write NewPathNotFoundError error: %w", err)
 	}
+
 	delivery.logger.Noticef("board id: %v not exist but be queried, info: %v err: %v", boardID, item, err)
 }
 
+// parseBoardPath covert url path from /v1/boards/SYSOP/article to
+// {SYSOP, article) or /v1/boards to {,}
 func (delivery *Delivery) parseBoardPath(path string) (boardID string, item string, filename string, err error) {
 	pathSegment := strings.Split(path, "/")
+
 	if len(pathSegment) >= 6 {
-		return pathSegment[3], pathSegment[4], pathSegment[5], nil
-	}
-	if len(pathSegment) == 5 {
-		return pathSegment[3], pathSegment[4], "", nil
-	}
-	if len(pathSegment) == 4 {
-		return pathSegment[3], "", "", nil
-	}
-	if len(pathSegment) == 3 {
-		return "", "", "", nil
+		// /{{version}}/boards/{{class_id}}/{{item}}/{{filename}}
+		boardID = pathSegment[3]
+		item = pathSegment[4]
+		filename = pathSegment[5]
+		return
+	} else if len(pathSegment) == 5 {
+		// /{{version}}/boards/{{class_id}}/{{item}}
+		boardID = pathSegment[3]
+		item = pathSegment[4]
+		return
+	} else if len(pathSegment) == 4 {
+		// /{{version}}/boards/{{class_id}}
+		boardID = pathSegment[3]
+		return
+	} else if len(pathSegment) == 3 {
+		// /{{version}}/boards
+		// Should not be reach...
+		return
 	}
 	delivery.logger.Warningf("parseBoardPath got malform path: %v", path)
-	return "", "", "", nil
+	return
 }
 
+// parseBoardTreasurePath parse covert url path from /v1/boards/SYSOP/article to
+// {SYSOP, article) or /v1/boards to {,}
 func (delivery *Delivery) parseBoardTreasurePath(path string) (boardID string, treasuresID []string, filename string, err error) {
 	pathSegment := strings.Split(path, "/")
+
 	if len(pathSegment) == 6 {
-		return pathSegment[3], []string{}, "", nil
-	}
-	if len(pathSegment) >= 7 {
+		// /{{version}}/boards/{{board_id}}/treasures/articles
+		boardID = pathSegment[3]
+		treasuresID = []string{}
+		filename = ""
+		return
+	} else if len(pathSegment) >= 7 {
+		// /{{version}}/boards/{{board_id}}/treasures/{{treasures_id ... }}/articles
+		// or
+		// /{{version}}/boards/{{board_id}}/treasures/{{treasures_id ... }}/articles/{{filename}}
 		boardID = pathSegment[3]
 		if pathSegment[len(pathSegment)-1] == "articles" {
-			return boardID, pathSegment[5 : len(pathSegment)-1], "", nil
+			treasuresID = pathSegment[5 : len(pathSegment)-1]
+			filename = ""
+		} else {
+			treasuresID = pathSegment[5 : len(pathSegment)-2]
+			filename = pathSegment[len(pathSegment)-1]
 		}
-		return boardID, pathSegment[5 : len(pathSegment)-2], pathSegment[len(pathSegment)-1], nil
+		return
 	}
+	// should not be reached
 	delivery.logger.Warningf("parseBoardTreasurePath got malform path: %v", path)
 	return
 }
 
+// parseClassPath covert url path from /v1/classes/1/information to
+// {1, information) or /v1/classes to {,}
 func (delivery *Delivery) parseClassPath(path string) (classID string, item string, err error) {
 	pathSegment := strings.Split(path, "/")
 	if len(pathSegment) == 5 {
+		// /{{version}}/classes/{{class_id}}/{{item}}
 		return pathSegment[3], pathSegment[4], nil
-	}
-	if len(pathSegment) == 4 {
+	} else if len(pathSegment) == 4 {
+		// /{{version}}/classes/{{class_id}}
 		return pathSegment[3], "", nil
-	}
-	if len(pathSegment) == 3 {
+	} else if len(pathSegment) == 3 {
+		// /{{version}}/classes
 		return "", "", nil
 	}
 	delivery.logger.Warningf("parseClassPath got malform path: %v", path)
@@ -241,17 +284,20 @@ func (delivery *Delivery) parseClassPath(path string) (classID string, item stri
 func parseUserPath(path string) (userID string, item string, itemID string, err error) {
 	pathSegment := strings.Split(path, "/")
 	if len(pathSegment) == 6 {
+		// /{{version}}/users/{{user_id}}/{{item}}/{{itemID}}
 		return pathSegment[3], pathSegment[4], pathSegment[5], nil
-	}
-	if len(pathSegment) == 5 {
+	} else if len(pathSegment) == 5 {
+		// /{{version}}/users/{{user_id}}/{{item}}
 		return pathSegment[3], pathSegment[4], "", nil
-	}
-	if len(pathSegment) == 4 {
+	} else if len(pathSegment) == 4 {
+		// /{{version}}/users/{{user_id}}
 		return pathSegment[3], "", "", nil
 	}
 	return "", "", "", nil
 }
 
+// return a boolean value to indicate support guest account
+// and using guest permission when permission insufficient
 func supportGuest() bool {
 	return false
 }
